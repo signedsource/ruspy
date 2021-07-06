6207223ea45ae6fb01c3eb35f7695e083bd510fc
Aaron's Dev <super.conctacto@gmail.com>
2021-07-06 20:31:07 +0200

    Fix the int parsing but not the string parsing

--- src/parser/Parser.js
+++ src/parser/Parser.js
@@ -3,6 +3,9 @@ const tokenParser = require("./tokens/TokenParser");
 const parser = async out => {
     out = out.split("'")
 
+    out = out.join(" ");
+    out = out.split(" ");
+
     out.forEach((char, i) => {
         if (out.indexOf(out[i]) !== 0 || out.indexOf(out[i]) !== out.length) {
             if (out[i].startsWith(" ") || out[i].endsWith(" ")) {
