0549b6abcea372703d85e11e59a9febc5613b4e0
Aaron's Dev <super.conctacto@gmail.com>
2021-07-06 23:05:29 +0200

    Make some changes

--- src/compiler/config/Config.js
+++ src/compiler/config/Config.js
@@ -1,3 +1,5 @@
 let version = "0.1.0";
+let welcome_message = "Welcome to Ruspy\nType '.help' to view the avalibable commands"
 
 module.exports = version;
+module.exports = welcome_message;
--- src/shell/Shell.js
+++ src/shell/Shell.js
@@ -1,11 +1,13 @@
 const readline = require('readline');
-const version = require('../compiler/config/Config');
+const { version, welcome_message } = require('../compiler/config/Config');
 const parser = require('../parser/Parser');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+console.log(welcome_message);
 
 const ask = () => {
     rl.question(`RusPy (${version})> `, out => {
