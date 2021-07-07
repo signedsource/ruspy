fd2cbaa0a18b2c6290c3d14c513105c6950dcadb
Aaron's Dev <super.conctacto@gmail.com>
2021-07-07 16:38:39 +0200

    Fix an error that will trigger if you have a whitline on the shell

--- src/parser/Parser.js
+++ src/parser/Parser.js
@@ -2,11 +2,15 @@ const tokenParser = require("./tokens/TokenParser");
 const log = require("../functions/Log");
 
 const parser = async out => {
-    out = out.match(/\S+|"[^"]+"/g)
+    if (out == "") {
+        return;
+    } else {
+        out = out.match(/\S+|"[^"]+"/g)
 
-    out.forEach(async e => {
-        log(await tokenParser(e));
-    });
+        out.forEach(async e => {
+            log(await tokenParser(e));
+        });
+    }
 }
 
 module.exports = parser;
